9cb44e835926acbdd908494dda4b9d4e3746d547
Veronica P <vhayami@gmail.com>
2022-05-30 19:42:03 +0300

    fix: add alt according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="task-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='';
     deleteButton.appendChild(deleteButtonImg);
 
 
